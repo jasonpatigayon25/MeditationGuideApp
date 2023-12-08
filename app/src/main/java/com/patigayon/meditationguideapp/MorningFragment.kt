092469230454f4dc14fa403c9a5a5bf162a2362f
Markymy <rocabertemarky09@gmail.com>
2023-12-08 19:51:53 +0800

    project

--- app/src/main/java/com/patigayon/meditationguideapp/MorningFragment.kt
+++ app/src/main/java/com/patigayon/meditationguideapp/MorningFragment.kt
@@ -5,12 +5,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
 import com.patigayon.meditationguideapp.databinding.FragmentMorningBinding
 
 class MorningFragment : Fragment() {
 
     private var _binding: FragmentMorningBinding? = null
     private val binding get() = _binding!!
+
+    private val db = FirebaseFirestore.getInstance()
+    private var morningMeditations = mutableListOf<MeditationTechnique>()
+    private lateinit var meditationAdapter: MeditationAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -22,7 +28,30 @@ class MorningFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        // Initialize your UI components here
+        setupRecyclerView()
+        fetchMorningMeditationsFromFirestore()
+    }
+
+    private fun setupRecyclerView() {
+        meditationAdapter = MeditationAdapter(morningMeditations) { technique ->
+            // handle click on meditation technique
+        }
+        binding.recyclerMorningMeditations.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerMorningMeditations.adapter = meditationAdapter
+    }
+
+    private fun fetchMorningMeditationsFromFirestore() {
+        db.collection("meditations")
+            .whereEqualTo("routine", "Morning Routine") // Filter by the 'routine' field
+            .get()
+            .addOnSuccessListener { documents ->
+                morningMeditations.clear()
+                morningMeditations.addAll(documents.mapNotNull { it.toObject(MeditationTechnique::class.java) })
+                meditationAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                // Handle error, possibly show a message to the user
+            }
     }
 
     override fun onDestroyView() {
