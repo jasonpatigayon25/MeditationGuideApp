07ff2e17f765ca46eab6b88ab03ba0c196d0216d
Markymy <rocabertemarky09@gmail.com>
2023-12-08 17:26:30 +0800

    project

--- app/src/main/java/com/patigayon/meditationguideapp/HomeFragment.kt
+++ app/src/main/java/com/patigayon/meditationguideapp/HomeFragment.kt
@@ -1,18 +1,99 @@
 package com.patigayon.meditationguideapp
 
+import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.firebase.firestore.FirebaseFirestore
+import com.patigayon.meditationguideapp.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+    private val db = FirebaseFirestore.getInstance()
+    private var meditationTechniques = mutableListOf<MeditationTechnique>()
+    private lateinit var meditationAdapter: MeditationAdapter
+    private lateinit var categoryAdapter: CategoryAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setupRecyclerView()
+        fetchMeditationsFromFirestore()
+        setupCategoryRecyclerView()
+    }
+
+    private fun onCategoryClicked(category: Category) {
+        Toast.makeText(context, "Category Clicked: ${category.title}", Toast.LENGTH_SHORT).show()
+    }
+
+    private fun setupCategoryRecyclerView() {
+        categoryAdapter = CategoryAdapter(emptyList(), this::onCategoryClicked)
+        binding.recyclerCategories.adapter = categoryAdapter
+        binding.recyclerCategories.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        fetchCategoriesFromFirestore()
+    }
+
+    private fun fetchCategoriesFromFirestore() {
+        db.collection("categories")
+            .get()
+            .addOnSuccessListener { documents ->
+                val categories = documents.mapNotNull { it.toObject(Category::class.java) }
+                categoryAdapter.updateCategories(categories)
+                categoryAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, "Error fetching categories", exception)
+            }
+    }
+
+    private fun setupRecyclerView() {
+        meditationAdapter = MeditationAdapter(meditationTechniques) { technique ->
+            val intent = Intent(context, DetailActivity::class.java).apply {
+                putExtra("name", technique.name)
+                putExtra("routine", technique.routine)
+                putExtra("photo", technique.photo)
+                putExtra("description", technique.description)
+            }
+            startActivity(intent)
+        }
+        binding.recyclerMeditationTechniques.layoutManager = LinearLayoutManager(context, LinearLayoutManager.HORIZONTAL, false)
+        binding.recyclerMeditationTechniques.adapter = meditationAdapter
+    }
+
+    private fun fetchMeditationsFromFirestore() {
+        db.collection("meditations")
+            .get()
+            .addOnSuccessListener { documents ->
+                meditationTechniques.clear()
+                meditationTechniques.addAll(documents.mapNotNull { it.toObject(MeditationTechnique::class.java) })
+                meditationAdapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, "Error fetching meditations", exception)
+            }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    companion object {
+        private const val TAG = "HomeFragment"
     }
 }
--- app/src/main/java/com/patigayon/meditationguideapp/MainActivity.kt
+++ app/src/main/java/com/patigayon/meditationguideapp/MainActivity.kt
@@ -1,14 +1,8 @@
 package com.patigayon.meditationguideapp
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.view.Window
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
-import androidx.recyclerview.widget.LinearLayoutManager
-import com.bumptech.glide.Glide
 import com.google.firebase.firestore.FirebaseFirestore
 import com.patigayon.meditationguideapp.databinding.ActivityMainBinding
 
@@ -21,76 +15,21 @@ class MainActivity : AppCompatActivity() {
     private lateinit var categoryAdapter: CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        categoryAdapter = CategoryAdapter(emptyList(), this::onCategoryClicked)
-        requestWindowFeature(Window.FEATURE_NO_TITLE)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setupRecyclerView()
-        fetchMeditationsFromFirestore()
         setupBottomNavigationView()
-        setupCategoryRecyclerView()
-    }
-
-    private fun onCategoryClicked(category: Category) {
-        Toast.makeText(this, "Category Clicked: ${category.title}", Toast.LENGTH_SHORT).show()
-    }
-
-    private fun setupCategoryRecyclerView() {
-        categoryAdapter = CategoryAdapter(emptyList()) { category ->
-            Toast.makeText(this, "Clicked on ${category.title}", Toast.LENGTH_SHORT).show()
+        if (savedInstanceState == null) {
+            replaceFragment(HomeFragment())  // Load HomeFragment as default
         }
-        binding.recyclerCategories.adapter = categoryAdapter
-        binding.recyclerCategories.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-
-        fetchCategoriesFromFirestore()
-    }
-
-    private fun fetchCategoriesFromFirestore() {
-        db.collection("categories")
-            .get()
-            .addOnSuccessListener { documents ->
-                val categories = documents.mapNotNull { it.toObject(Category::class.java) }
-                categoryAdapter.updateCategories(categories)
-                categoryAdapter.notifyDataSetChanged()
-            }
-            .addOnFailureListener { exception ->
-                Log.e(TAG, "Error fetching categories", exception)
-            }
-    }
-
-    private fun setupRecyclerView() {
-        meditationAdapter = MeditationAdapter(meditationTechniques) { technique ->
-            val intent = Intent(this, DetailActivity::class.java).apply {
-                putExtra("name", technique.name)
-                putExtra("routine", technique.routine)
-                putExtra("photo", technique.photo)
-                putExtra("description", technique.description)
-            }
-            startActivity(intent)
-        }
-        binding.recyclerMeditationTechniques.layoutManager = LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false)
-        binding.recyclerMeditationTechniques.adapter = meditationAdapter
-    }
-
-    private fun fetchMeditationsFromFirestore() {
-        db.collection("meditations")
-            .get()
-            .addOnSuccessListener { documents ->
-                meditationTechniques.clear()
-                meditationTechniques.addAll(documents.mapNotNull { it.toObject(MeditationTechnique::class.java) })
-                meditationAdapter.notifyDataSetChanged()
-            }
-            .addOnFailureListener { exception ->
-                Log.e(TAG, "Error fetching meditations", exception)
-            }
     }
 
     private fun setupBottomNavigationView() {
         binding.bottomNavigation.setOnItemSelectedListener { item ->
             when (item.itemId) {
                 R.id.navigation_home -> {
+                    replaceFragment(HomeFragment())
                     true
                 }
                 R.id.navigation_discover -> {
