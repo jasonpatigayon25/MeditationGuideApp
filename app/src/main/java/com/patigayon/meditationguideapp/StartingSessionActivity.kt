e932c450ef3e2e9dbf6ab2337e8e658d72c1aa8c
Markymy <rocabertemarky09@gmail.com>
2023-12-13 12:12:33 +0800

    project and exam for finals

--- app/src/main/java/com/patigayon/meditationguideapp/StartingSessionActivity.kt
+++ app/src/main/java/com/patigayon/meditationguideapp/StartingSessionActivity.kt
@@ -1,9 +1,12 @@
 package com.patigayon.meditationguideapp
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
 import android.os.CountDownTimer
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import com.patigayon.meditationguideapp.databinding.ActivityStartingSessionBinding
 
 class StartingSessionActivity : AppCompatActivity() {
@@ -11,50 +14,81 @@ class StartingSessionActivity : AppCompatActivity() {
     private var countDownTimer: CountDownTimer? = null
     private var timerRunning = false
     private var timeLeftInMillis: Long = 5 * 60000
+    private var colorAnimation: ObjectAnimator? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        try {
-            supportActionBar!!.hide()
-        } catch (e: NullPointerException) {
-            e.printStackTrace()
-        }
+        supportActionBar?.hide()
 
         binding = ActivityStartingSessionBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.timePicker.setIs24HourView(true)
-        binding.timePicker.currentHour = 0
-        binding.timePicker.currentMinute = 5
-
-        binding.startButton.visibility = View.VISIBLE
-        binding.stopButton.visibility = View.GONE
-
-        binding.backButton.setOnClickListener {
-            finish()
-        }
-
-        binding.nextButton.setOnClickListener {
-            //
-        }
+        setupTimePicker()
 
         binding.startButton.setOnClickListener {
-            timeLeftInMillis = binding.timePicker.currentMinute.toLong() * 60000
-            startTimer(timeLeftInMillis)
-
-            timerRunning = true
-            binding.startButton.visibility = View.GONE
-            binding.stopButton.visibility = View.VISIBLE
+            if (!timerRunning) {
+                val selectedTime = getSelectedTimeFromPicker()
+                timeLeftInMillis = selectedTime * 60000L
+                startTimer(timeLeftInMillis)
+                animateMeditationIcon()
+            }
         }
 
         binding.stopButton.setOnClickListener {
             countDownTimer?.cancel()
-            updateCountDownText(timeLeftInMillis)
             timerRunning = false
-            binding.startButton.visibility = View.VISIBLE
-            binding.stopButton.visibility = View.GONE
+            updateButtons()
+            stopMeditationIconAnimation()
         }
+
+        updateButtons()
+    }
+
+    private fun setupTimePicker() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.timePicker.apply {
+                setIs24HourView(true)
+                hour = 0
+                minute = 5
+            }
+        } else {
+            @Suppress("DEPRECATION")
+            binding.timePicker.apply {
+                setIs24HourView(true)
+                currentHour = 0
+                currentMinute = 5
+            }
+        }
+    }
+
+    private fun getSelectedTimeFromPicker(): Int {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            binding.timePicker.minute
+        } else {
+            @Suppress("DEPRECATION")
+            binding.timePicker.currentMinute
+        }
+    }
+
+    private fun animateMeditationIcon() {
+        colorAnimation = ObjectAnimator.ofArgb(
+            binding.meditationIcon.drawable.mutate(),
+            "tint",
+            ContextCompat.getColor(this, R.color.start_color),
+            ContextCompat.getColor(this, R.color.mid_color),
+            ContextCompat.getColor(this, R.color.end_color)
+        ).apply {
+            duration = 3000
+            repeatCount = ObjectAnimator.INFINITE
+            repeatMode = ObjectAnimator.REVERSE
+        }
+        colorAnimation?.start()
+    }
+
+    private fun stopMeditationIconAnimation() {
+        colorAnimation?.cancel()
+        binding.meditationIcon.clearColorFilter()
     }
 
     private fun startTimer(millisInFuture: Long) {
@@ -66,11 +100,12 @@ class StartingSessionActivity : AppCompatActivity() {
 
             override fun onFinish() {
                 timerRunning = false
-                binding.startButton.visibility = View.VISIBLE
-                binding.stopButton.visibility = View.GONE
-                updateCountDownText(0) // Clear the text
+                updateButtons()
+                updateCountDownText(0)
             }
         }.start()
+        timerRunning = true
+        updateButtons()
     }
 
     private fun updateCountDownText(timeInMillis: Long) {
@@ -78,5 +113,15 @@ class StartingSessionActivity : AppCompatActivity() {
         val seconds = (timeInMillis / 1000) % 60
         val timeFormatted = String.format("%02d:%02d", minutes, seconds)
         binding.timerTextView.text = timeFormatted
+    }
+
+    private fun updateButtons() {
+        if (timerRunning) {
+            binding.startButton.visibility = View.GONE
+            binding.stopButton.visibility = View.VISIBLE
+        } else {
+            binding.startButton.visibility = View.VISIBLE
+            binding.stopButton.visibility = View.GONE
+        }
     }
 }
